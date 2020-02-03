c9b60813b125816758b91d961c552e951e1584c3
LP-tulutub <347863282@qq.com>
2020-02-04 07:59:43 +0800

    SpringBoot-learn

--- _24-spring-boot-redis/src/test/java/com/lp/AnyTimeTest.java
+++ _24-spring-boot-redis/src/test/java/com/lp/AnyTimeTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -15,8 +16,10 @@ public class AnyTimeTest {
 
     @Test
     public void testGet(){
-        String value = (String)this.redisTemplate.opsForValue().get("7f1c8841-fac2-4e14-bfc1-82cfcece3cbb");
-        System.out.println(value);
+        this.redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        Object obj = this.redisTemplate.opsForValue().get("7f1c8841-fac2-4e14-bfc1-82cfcece3cbb1");
+
+        System.out.println(obj==null);
     }
 
 }
